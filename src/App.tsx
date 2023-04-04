aacd83cee7c0a046e6f11615844660a30a616853
qurriahSam <mugirethe1@gmail.com>
2023-04-05 02:15:19 +0300

    ch: react query setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import Navbar from "./components/Navbar";
 
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   );
 }
