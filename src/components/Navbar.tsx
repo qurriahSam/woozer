6a83f2b944ff437e48ca8d9da05d281f3bd05837
qurriahSam <mugirethe1@gmail.com>
2023-04-04 23:39:37 +0300

    ft: setup app navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className='bg-zinc-200 p-3'>
+      <p className="text-3xl font-['pacifico'] text-amber-500">Wooz!k</p>
+    </div>
+  );
+};
+
+export default Navbar;
