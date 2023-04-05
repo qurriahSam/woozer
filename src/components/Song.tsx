b1587437ba238629c0b79303829e6dbf50e1d89f
qurriahSam <mugirethe1@gmail.com>
2023-04-05 11:50:08 +0300

    ch: send clicked song to player

--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-import { SongInt } from "../types/types";
+import { PlayList, SongInt } from "../types/types";
 
 const Song = ({ id, title, artist, preview, img_medium, img_small, songPlay }: SongInt) => {
+  const handleClick = () => {
+    const playSong: PlayList = {
+      id: 1,
+      name: title,
+      writer: artist,
+      img: img_small,
+      src: preview,
+    };
+    songPlay(playSong);
+  };
+
   return (
-    <div>
+    <div onClick={handleClick}>
       <img src={img_small} alt='imd' />
       <p>
         {title} - {artist}
