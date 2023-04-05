7bd672780c2abba2d30167977cb1c12d5abe472f
qurriahSam <mugirethe1@gmail.com>
2023-04-05 10:28:14 +0300

    ft: fetch songs from api and display

---
+++ src/components/Home.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import fetchSongs from "../api/api-calls";
+import Song from "./Song";
+
+const Home = ({ songPlay }: { songPlay: (id: number, preview: string) => void }) => {
+  const { isError, isLoading, data, error } = useQuery(["posts"], fetchSongs);
+
+  if (isLoading) return <h1>Loading....</h1>;
+  if (isError) console.log(error);
+
+  if (isError) return <h1>Error loading data!!!</h1>;
+
+  return (
+    <div>
+      <ul>
+        {data.map((data) => (
+          <li key={data.id}>
+            <Song
+              id={data.id}
+              title={data.title}
+              artist={data.artist.name}
+              preview={data.preview}
+              img_small={data.album.cover_small}
+              img_medium={data.album.cover_medium}
+              songPlay={songPlay}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
