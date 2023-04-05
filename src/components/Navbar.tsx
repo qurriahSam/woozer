e93ff4827917f7eed113247e01a77b2e1e0e1a49
qurriahSam <mugirethe1@gmail.com>
2023-04-05 23:29:11 +0300

    st: add font awesome icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,13 +14,21 @@ const Navbar = ({ songSearch }: { songSearch: (search: string) => void }) => {
   };
 
   return (
-    <div className='bg-zinc-200 p-3 fixed top-0 left-0 right-0 flex justify-between'>
+    <div className='bg-zinc-200 px-5 py-3 fixed top-0 left-0 right-0 flex justify-between'>
       <p className="text-3xl font-['pacifico'] text-amber-500">Wooz!k</p>
 
-      <div className='self-center p-2 bg-white'>
+      <div className='self-center p-2 bg-white rounded-full'>
         <form onSubmit={handleSubmit}>
-          <input type='text' placeholder='Search' value={searchTerm} onChange={handleChange} />
-          <button>Search</button>
+          <input
+            className='focus:outline-0 ps-3'
+            type='text'
+            placeholder='Search'
+            value={searchTerm}
+            onChange={handleChange}
+          />
+          <button>
+            <i className='fas fa-search px-3 text-stone-500 hover:text-stone-900'></i>
+          </button>
         </form>
       </div>
       <div></div>
