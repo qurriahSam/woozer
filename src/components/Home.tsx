e4060ca9b5a59f0bf742bac5547be988e8bf8ea2
qurriahSam <mugirethe1@gmail.com>
2023-04-05 15:01:22 +0300

    ch: change from query to useEffect

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,33 +1,52 @@
-import React from "react";
-import { useQuery } from "@tanstack/react-query";
-import fetchSongs from "../api/api-calls";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Song from "./Song";
-import { PlayList } from "../types/types";
+import { PlayList, DeezerResp } from "../types/types";
 
 const Home = ({ songPlay }: { songPlay: (song: PlayList) => void }) => {
-  const { isError, isLoading, data, error } = useQuery(["posts"], fetchSongs);
+  const [apiSongs, setApiSongs] = useState<DeezerResp[]>([]);
+  const [error, setError] = useState<any>({});
 
-  if (isLoading) return <h1>Loading....</h1>;
-  if (isError) console.log(error);
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://deezerdevs-deezer.p.rapidapi.com/search",
+      params: { q: "sauti sol" },
+      headers: {
+        "X-RapidAPI-Key": "f16440cf3cmsh4be98b0308b4c28p17aa20jsn0ca4171aba9f",
+        "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+      },
+    };
 
-  if (isError) return <h1>Error loading data!!!</h1>;
+    axios
+      .request(options)
+      .then(function (response) {
+        setApiSongs(response.data.data);
+        console.log(response.data.data);
+      })
+      .catch(function (error) {
+        setError(error);
+      });
+  }, []);
 
   return (
     <div className='pt-16 pb-16 bg-slate-100'>
       <ul>
-        {data.map((data) => (
-          <li key={data.id}>
-            <Song
-              id={data.id}
-              title={data.title}
-              artist={data.artist.name}
-              preview={data.preview}
-              img_small={data.album.cover_small}
-              img_medium={data.album.cover_medium}
-              songPlay={songPlay}
-            />
-          </li>
-        ))}
+        {apiSongs.length > 1
+          ? apiSongs.map((data: any) => (
+              <li key={data.id}>
+                <Song
+                  id={data.id}
+                  title={data.title}
+                  artist={data.artist.name}
+                  preview={data.preview}
+                  img_small={data.album.cover_small}
+                  img_medium={data.album.cover_medium}
+                  songPlay={songPlay}
+                />
+              </li>
+            ))
+          : null}
       </ul>
     </div>
   );
